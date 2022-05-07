66c7894a8aa832ff6a4c8dfa5bd6b9a3c7d4c06c
Doron Tal <doron@tracktunes.org>
2022-05-07 05:44:29 -0400

    minor

--- lib/widgets/password_field.dart
+++ lib/widgets/password_field.dart
@@ -49,39 +49,32 @@ String? _validatePassword(String? password, [String? genericResponse]) {
   final RegExp hasLowercaseRegExp = RegExp(r'[a-z]+');
   final RegExp hasUppercaseRegExp = RegExp(r'[A-Z]+');
   final RegExp hasDigitsRegExp = RegExp(r'[0-9]+');
-  const String invalidMessage01 = 'Please enter a password';
-  const String invalidMessage02 =
-      'Password must have at least $minLength characters';
-  const String invalidMessage03 =
-      'Password must have at most $maxLength characters';
-  const String invalidMessage04 = 'Password must be all ASCII characters';
-  const String invalidMessage05 =
-      'Password must have at least one lowercase character';
-  const String invalidMessage06 =
-      'Password must have at least one uppercase character';
-  const String invalidMessage07 = 'Password must contain at least one number';
 
   if (password == null || password.isEmpty) {
-    return genericResponse ?? invalidMessage01;
+    return genericResponse ?? 'Please enter a password';
   } else {
     final length = password.length;
     if (length < minLength) {
-      return genericResponse ?? invalidMessage02;
+      return genericResponse ??
+          'Password must have at least $minLength characters';
     }
     if (length > maxLength) {
-      return genericResponse ?? invalidMessage03;
+      return genericResponse ??
+          'Password must have at most $maxLength characters';
     }
     if (nonASCIIRegExp.hasMatch(password)) {
-      return genericResponse ?? invalidMessage04;
+      return genericResponse ?? 'Password must be all ASCII characters';
     }
     if (!hasLowercaseRegExp.hasMatch(password)) {
-      return genericResponse ?? invalidMessage05;
+      return genericResponse ??
+          'Password must have at least one lowercase character';
     }
     if (!hasUppercaseRegExp.hasMatch(password)) {
-      return genericResponse ?? invalidMessage06;
+      return genericResponse ??
+          'Password must have at least one uppercase character';
     }
     if (!hasDigitsRegExp.hasMatch(password)) {
-      return genericResponse ?? invalidMessage07;
+      return genericResponse ?? 'Password must contain at least one number';
     }
   }
   return null;
