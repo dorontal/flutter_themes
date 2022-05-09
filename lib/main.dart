0aa70d8f51122383a484afcdfdc35f55439930ea
Doron Tal <doron@tracktunes.org>
2022-05-09 18:12:06 -0400

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -29,25 +29,25 @@ class FormPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _formKey = useMemoized(() => GlobalKey<FormState>());
-    final _emailFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
-    final _passwordFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
-    final _codeFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
-    final _titleFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+    final formKey = useMemoized(() => GlobalKey<FormState>());
+    final emailFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+    final passwordFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+    final codeFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+    final titleFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Theme playground'),
       ),
       body: Form(
-        key: _formKey,
+        key: formKey,
         child: ListView(
           padding: const EdgeInsets.all(32),
           children: <Widget>[
-            EmailField(fieldKey: _emailFieldKey),
-            PasswordField(fieldKey: _passwordFieldKey),
-            CodeField(fieldKey: _codeFieldKey),
-            TitleField(fieldKey: _titleFieldKey),
+            EmailField(fieldKey: emailFieldKey),
+            PasswordField(fieldKey: passwordFieldKey),
+            CodeField(fieldKey: codeFieldKey),
+            TitleField(fieldKey: titleFieldKey),
           ]
               .map((child) => Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8),
@@ -58,12 +58,12 @@ class FormPage extends HookWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          final _valid = _formKey.currentState!.validate();
+          final _valid = formKey.currentState!.validate();
           dev.log('Valid form?: $_valid');
-          dev.log('email: ${_emailFieldKey.currentState!.value}');
-          dev.log('password: ${_passwordFieldKey.currentState!.value}');
-          dev.log('code: ${_codeFieldKey.currentState!.value}');
-          dev.log('title: ${_titleFieldKey.currentState!.value}');
+          dev.log('email: ${emailFieldKey.currentState!.value}');
+          dev.log('password: ${passwordFieldKey.currentState!.value}');
+          dev.log('code: ${codeFieldKey.currentState!.value}');
+          dev.log('title: ${titleFieldKey.currentState!.value}');
         },
         tooltip: 'Check validity',
         child: const Icon(Icons.check),
