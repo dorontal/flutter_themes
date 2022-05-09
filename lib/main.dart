3e875b57bf6e6cb29686de5a17ccfe72ecb3cfc5
Doron Tal <doron@tracktunes.org>
2022-05-09 12:25:35 -0400

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -27,45 +27,47 @@ class MyApp extends StatelessWidget {
 class FormPage extends HookWidget {
   FormPage({Key? key}) : super(key: key);
 
-  final _formKey = GlobalKey<FormState>();
-  final _emailFieldKey = GlobalKey<FormFieldState>();
-  final _passwordFieldKey = GlobalKey<FormFieldState>();
-  final _codeFieldKey = GlobalKey<FormFieldState>();
-  final _titleFieldKey = GlobalKey<FormFieldState>();
-
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Theme playground'),
+  Widget build(BuildContext context) {
+    final _formKey = useMemoized(() => GlobalKey<FormState>());
+    final _emailFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+    final _passwordFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+    final _codeFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+    final _titleFieldKey = useMemoized(() => GlobalKey<FormFieldState>());
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme playground'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(32),
+          children: <Widget>[
+            EmailField(fieldKey: _emailFieldKey),
+            PasswordField(fieldKey: _passwordFieldKey),
+            CodeField(fieldKey: _codeFieldKey),
+            TitleField(fieldKey: _titleFieldKey),
+          ]
+              .map((child) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: child,
+                  ))
+              .toList(),
         ),
-        body: Form(
-          key: _formKey,
-          child: ListView(
-            padding: const EdgeInsets.all(32),
-            children: <Widget>[
-              EmailField(fieldKey: _emailFieldKey),
-              PasswordField(fieldKey: _passwordFieldKey),
-              CodeField(fieldKey: _codeFieldKey),
-              TitleField(fieldKey: _titleFieldKey),
-            ]
-                .map((child) => Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: child,
-                    ))
-                .toList(),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            final _valid = _formKey.currentState!.validate();
-            dev.log('Valid form?: $_valid');
-            dev.log('email: ${_emailFieldKey.currentState!.value}');
-            dev.log('password: ${_passwordFieldKey.currentState!.value}');
-            dev.log('code: ${_codeFieldKey.currentState!.value}');
-            dev.log('title: ${_titleFieldKey.currentState!.value}');
-          },
-          tooltip: 'Check validity',
-          child: const Icon(Icons.check),
-        ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final _valid = _formKey.currentState!.validate();
+          dev.log('Valid form?: $_valid');
+          dev.log('email: ${_emailFieldKey.currentState!.value}');
+          dev.log('password: ${_passwordFieldKey.currentState!.value}');
+          dev.log('code: ${_codeFieldKey.currentState!.value}');
+          dev.log('title: ${_titleFieldKey.currentState!.value}');
+        },
+        tooltip: 'Check validity',
+        child: const Icon(Icons.check),
+      ),
+    );
+  }
 }
